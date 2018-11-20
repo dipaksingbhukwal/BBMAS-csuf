15801f96ebb98cda23c00cc57cc8317e13c198e4
dipaksingbhukwal <35825317+dipaksingbhukwal@users.noreply.github.com>
2018-11-19 18:06:48 -0800

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
 console.log(process.NODE_ENV);
   console.log('production');
-  module.exports = {mongoURI: 'mongodb://crud:abcd1234@ds157853.mlab.com:57853/vidjot-csuf-prod'}
+  module.exports = {mongoURI: require('./database_prod').mongoURI}
 } else {
   console.log('dev');
   module.exports = {mongoURI: 'mongodb://localhost/bbmas-dev'}
